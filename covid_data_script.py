56dd914d6558f40116db468f5660aa0aea75075d
JoshuaClugston <91908171+JoshuaClugston@users.noreply.github.com>
2022-12-03 10:29:09 -0700

    Update covid_data_script.py

--- covid_data_script.py
+++ covid_data_script.py
@@ -12,7 +12,7 @@ covid_df = pd.read_csv(r'covid_data.csv')
 '''
 according to CDC (https://www.cdc.gov/museum/timeline/covid19.html#:~:t
 ext=January%2020%2C%202020,respond%20to%20the%20emerging%20outbreak.),
-fist officially (laboratory) reported case in US was discovered on
+first officially (laboratory) reported case in US was discovered on
 2020-20-2020. This information is used to define orig_date below.
 '''
 
@@ -31,10 +31,6 @@ for ind in covid_df.index:
 
 covid_df['State of emergency declared'] = day_lst
 covid_df.to_csv('covid_data_updated.csv')
-
-   
-# note: one value (Feb-29) was changed manually to 29-Feb
-# to match the other values' pattern
 
 
 
