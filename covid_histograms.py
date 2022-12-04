83ff5d3f75eb4ad16257de6fca530008141af583
JoshuaClugston <91908171+JoshuaClugston@users.noreply.github.com>
2022-12-04 11:05:53 -0700

    Update covid_histograms.py

--- covid_histograms.py
+++ covid_histograms.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import os
 
 covid_df = pd.read_csv(r'covid_data_updated.csv')
 covid_df = covid_df.drop(['Unnamed: 0', 'State'], axis=1)
 
+# create new folder for images
+os.mkdir('hist_img/')
 
 for column in covid_df.columns:
     sns.displot(data=covid_df[column], kde=True, color='red')
     plt.title(f'Histogram for {column}')
+    column_name = column.string()
+    plt.savefig(f'hist_img/{column_name}')
     plt.show()
